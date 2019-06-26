6d3406cfd9e27185d182c57309f17498c485ebdf
wusiq <dwusiq@163.c0m>
2019-06-26 15:51:56 +0800

    添加网关服务

--- service-consumer/src/main/java/com/wusiq/serviceconsumer/user/UserRemote.java
+++ service-consumer/src/main/java/com/wusiq/serviceconsumer/user/UserRemote.java
@@ -4,12 +4,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "service-provider")
+@FeignClient(name = "cloud-service-provider")
 public interface UserRemote {
 
-    @GetMapping("/cloud-service-provider/{userId}/userName")
+    @GetMapping("/{userId}/userName")
     String getUserName(@PathVariable Integer userId);
 
-    @GetMapping("/cloud-service-provider/time")
+    @GetMapping("/time")
     String getTime();
 }
